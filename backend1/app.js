4f14cce4c841d3e0f8499b1b98a60195c1c47c06
Deepak raj <97690607+deepak039@users.noreply.github.com>
2024-10-16 07:41:48 +0530

    models created Booking Center Courts Sport User

--- backend1/app.js
+++ backend1/app.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
 
 
 
-
-const PORT =  5000;
-app.listen( () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
